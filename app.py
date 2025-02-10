73a6c8f8a90a0df105f2fe5867c6ac16b611a6b6
Hardik Sharma <hs3052006@gmail.com>
2025-02-11 00:28:00 +0530

    Add home route and index.html template

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) 
 
 @app.route("/")
-def hello_world():
-  return "hiiiii innocreaters"
+@app.route("/home")
+def home():
+  return render_template("index.html")
 
 @app.route("/about")
 def about():
